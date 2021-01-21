65cf5f28505c5b2fe1a72c02844ac3543f920894
suryanarayan.rath <suryanarayan.rath@highradius.com>
2021-01-21 13:06:34 +0530

    Complete setup for Spring MVC

---
+++ src/main/java/com/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/home")
+	public String home(Model model) {
+		String home = "home";
+		return home;
+	}
+}
