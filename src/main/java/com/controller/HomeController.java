5f69e0584ac2c9eb772139cc3912b47e0ac121c0
suryanarayan.rath <suryanarayan.rath@highradius.com>
2021-01-28 21:50:02 +0530

    Mapping made for the Hibernate and Transaction

--- src/main/java/com/controller/HomeController.java
+++ src/main/java/com/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
 public class HomeController extends AbstractController {
+	
 	@Override
 	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
