409c60bf79f1d0c974815294364a974348951101
suryanarayan.rath <suryanarayan.rath@highradius.com>
2021-01-29 20:03:53 +0530

    Added Category entity

---
+++ src/main/java/com/entity/Category.java
@@ -0,0 +1,34 @@
+package com.entity;
+
+public class Category {
+	private int categoryId;
+	private String categoryName;
+	private String categoryDesc;
+	public Category() {
+		super();
+	}
+	public Category(int categoryId, String categoryName, String categoryDesc) {
+		super();
+		this.categoryId = categoryId;
+		this.categoryName = categoryName;
+		this.categoryDesc = categoryDesc;
+	}
+	public int getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	public String getCategoryDesc() {
+		return categoryDesc;
+	}
+	public void setCategoryDesc(String categoryDesc) {
+		this.categoryDesc = categoryDesc;
+	}
+}
